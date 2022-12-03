5acd33e93b565a0e56a60bc1968071c63775746b
Skylake143 <45364723+Skylake143@users.noreply.github.com>
2022-12-03 20:44:07 +0100

    [mod] Comments and Input

--- MatrixMultiplicator/Matrix.cpp
+++ MatrixMultiplicator/Matrix.cpp
@@ -148,6 +148,7 @@ Matrix Matrix::matrixmultiplicationSingleThreaded(Matrix matrixB)
     exit(1);
 }
 
+//Struct to store data needed for threading 
 struct thread_data{
     Matrix* output;
     int32_t** matrix;
@@ -157,6 +158,7 @@ struct thread_data{
 
 };
 
+//Matrix multiplication algorithm
 void* threadMatrixCalculation(void *threadarg)
 {
     struct thread_data *my_data;
@@ -170,13 +172,11 @@ void* threadMatrixCalculation(void *threadarg)
     //Iteration through columns of new matrix
     for(size_t matX=0; matX<output->dimX;++matX)
     {
-        //calculating Matrix result
+        //Variable for calculation of every single entry
         int32_t result = 0;
-        //Iterating again through x to calculate 
+        //Iterating again through x to calculate partly result
         for(size_t x=0; x<dimX;x++)
         {
-            int32_t test1 = matrix[y][x];
-            int32_t test2 = matrixB->matrix[x][matX];
             int32_t multipliedValue=matrix[y][x]*matrixB->matrix[x][matX];
             result+=multipliedValue;
         }
@@ -186,8 +186,65 @@ void* threadMatrixCalculation(void *threadarg)
     return NULL;
 }
 
-//Matrix-Multiplication Method single threaded
+//Matrix-Multiplication Method Multi threaded
 Matrix Matrix::matrixmultiplicationMultiThreaded(Matrix matrixB)
+{
+    //Check if Matrices have the correct dimensions
+    if(this->dimX != matrixB.dimY)
+    {
+        cout << "X-Dimension of Matrix A must be equal to Y-Dimentsion of Matrix B" << endl;
+        exit(1);
+    }
+    //Define new output Matrix where calculated entries are stored
+    Matrix output;
+    output.dimX = matrixB.dimX;
+    output.dimY = dimY;
+    //In C/C++ Matrices must be initialized with dynamic array
+    output.matrix = new int32_t * [output.dimY];
+
+    //Create array of type pthread_t
+    //Length of rows of the output array
+    //Each row will be calculated by a own thread
+    pthread_t threadArr[output.dimY];
+    //Create array of type struct thread_data to store data in
+    struct thread_data thread_data_array[output.dimY];
+    //Set up variable for result_code
+    int result_code;
+
+    //Iteration through lines of output array to initialize array
+    for(size_t y=0;y<output.dimY;++y)
+    {
+        output.matrix[y] = new int32_t[output.dimX];
+    }
+    //Iteration through lines of new matrix
+    for(size_t y=0;y<output.dimY;++y)
+    {
+        //Prepare thread_data with provided data for matrix multiplication
+        thread_data_array[y].matrixB = &matrixB;
+        thread_data_array[y].matrix = matrix;
+        thread_data_array[y].output = &output;
+        thread_data_array[y].y = y;
+        thread_data_array[y].dimX = dimX;
+
+        //Create threads by passing pthread_t object of every entry in array, function and pointer on provided data
+        result_code = pthread_create(&threadArr[y], NULL,threadMatrixCalculation,(void *)&thread_data_array[y]);
+        //Check if pthread has been successful
+        assert(!result_code);
+    }
+    //Set all threads in the mode to wait for all threads until they finished before continuing with pthread_join()
+    for(size_t y=0;y<output.dimY;++y)
+    {
+        result_code = pthread_join(threadArr[y], NULL);
+        //Check again for success
+        assert(!result_code);
+    }
+    
+    //Return calculated matrix
+    return output;
+}
+
+//Matrix-Multiplication Method Multi threaded with manual definition of scheduler
+Matrix Matrix::matrixmultiplicationMultiThreadedScheduler(Matrix matrixB, int scheduler, bool randPriority)
 {
     if(this->dimX != matrixB.dimY)
     {
@@ -218,89 +275,81 @@ Matrix Matrix::matrixmultiplicationMultiThreaded(Matrix matrixB)
         thread_data_array[y].y = y;
         thread_data_array[y].dimX = dimX;
 
-        result_code = pthread_create(&threadArr[y], NULL,threadMatrixCalculation,(void *)&thread_data_array[y]);
-        assert(!result_code);
-    }
-    for(size_t y=0;y<output.dimY;++y)
-    {
-        result_code = pthread_join(threadArr[y], NULL);
-        assert(!result_code);
-    }
-    
-    return output;
-}
-
-//Matrix-Multiplication Method single threaded
-Matrix Matrix::matrixmultiplicationMultiThreadedScheduler(Matrix matrixB, int scheduler, bool randPriority)
-{
-    if(this->dimX != matrixB.dimY)
-    {
-        cout << "X-Dimension of Matrix A must be equal to Y-Dimentsion of Matrix B" << endl;
-        exit(1);
-    }
-    //Define new output Matrix
-    Matrix output;
-    output.dimX = matrixB.dimX;
-    output.dimY = dimY;
-    output.matrix = new int32_t * [output.dimY];
-
-    pthread_t threadArr[output.dimY];
-    struct thread_data thread_data_array[output.dimY];
-
-    //Iteration through lines of new matrix
-    for(size_t y=0;y<output.dimY;++y)
-    {
-        output.matrix[y] = new int32_t[output.dimX];
-    }
-    //Iteration through lines of new matrix
-    for(size_t y=0;y<output.dimY;++y)
-    {
-        thread_data_array[y].matrixB = &matrixB;
-        thread_data_array[y].matrix = matrix;
-        thread_data_array[y].output = &output;
-        thread_data_array[y].y = y;
-        thread_data_array[y].dimX = dimX;
-
-        //Set scheduler
+        //Set scheduling parameters
         sched_param schedparam;
+        //Set scheduling priority
         if(randPriority == true)schedparam.sched_priority = std::rand() %10;
-        
+        //Manually add p_thread_attr_t
         pthread_attr_t attr;
         pthread_attr_init(&attr);
+
+        //Specify how thread scheduling attributes are defined
+        //PTHREAD_EXPLICIT_SCHED specifies that new thread gets scheduling attribute defined in this attribute
         pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+        //Set scheduling policy
         pthread_attr_setschedpolicy(&attr, SCHED_RR);
+        //Link sched_param to pthread_attr_t
         pthread_attr_setschedparam(&attr, &schedparam);
 
-        int rc = pthread_create(&threadArr[y], &attr,threadMatrixCalculation,(void *)&thread_data_array[y]);
+        //Continues as previously explained
+        result_code = pthread_create(&threadArr[y], &attr,threadMatrixCalculation,(void *)&thread_data_array[y]);
+        assert(!result_code);
         pthread_attr_destroy(&attr);
-        //TODO: int rc
     }
     for(size_t y=0;y<output.dimY;++y)
     {
         pthread_join(threadArr[y], NULL);
     }
-    //pthread_exit(NULL);
     
     return output;
 }
 
 int main()
 {
-    //Initialize matrixA
-    Matrix matrixA(1500,1500);
+    cout << "Do you want to calculate a quadratic Matrix multiplication: (yes/no)";
+    char input[]="";
+    cin >> input;
+    string quadratic;
+    for (int x=0; x<strlen(input); x++)
+        quadratic.push_back(toupper(input[x]));
+
+    int matSizeX=0;
+    int matSizeY=0;
+    if(quadratic == "YES" || quadratic =="Y")
+    {
+        cout << "Put in size of Matrix:";
+        cin >> input;
+        matSizeX=stoi(input);
+        matSizeY=matSizeX;
+    }
+    else
+    {
+        cout << "Put in x-Dimension:";
+        cin >> input;
+        matSizeX=stoi(input);
+        cout << "Put in y-Dimension:";
+        cin >> input;
+        matSizeY=stoi(input);
+    }
+    if(max(matSizeX,matSizeY)<11)PRINT_MATRICES=true;
+
+    //Initialize matrixA and print if necessary
+    Matrix matrixA(matSizeX,matSizeY);
     if(PRINT_MATRICES==true)
     {
         cout << "Matrix A: " << endl;
         matrixA.printMatrix();
     }
+
     
-    //Initialize matrix B
-    Matrix matrixB(1500,1500);
+    //Initialize matrix B and print if necessary
+    Matrix matrixB(matSizeX,matSizeX);
     if(PRINT_MATRICES==true)
     {
         cout << "Matrix B: " << endl;
         matrixB.printMatrix();
     }
+
 
     auto startTime = chrono::high_resolution_clock::now();
     Matrix output = matrixA.matrixmultiplicationSingleThreaded(matrixB);
